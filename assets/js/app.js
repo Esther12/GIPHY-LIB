8896d5b531aadefe67215a0095fde237971e3d2b
Yating(Esther) Su <yaringsu@foxmail.com>
2019-07-17 13:31:50 -0400

    able to add pictures with button in html, still need to work on make the picture still and when click button it will be the same

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,50 +2,55 @@ var apiKey = "4tk6Du0rBvIooneYCYnhNOPkGvcLUnET";
 var q = "";
 var limit = "";
 var rating = "";
-
 var queryUrl = "";
-
-
 
 //get the input value from html
 function getButtonInfo(){
-    limit = $("#limitDisplay").val();
-    p = $("#pDisplay").val();
-    rating = $("#rating").val();
+    limit = $("#limitInput").val();
+    p = $("#pInput").val();
+    rating = $("#ratingInput").val();
 }
 
-
 //addgifbutton on click
-
 $("#addButton").on("click", function(){
-    debugger;
-    p = $("#pDisplay").val();
+    //debugger;
+    getButtonInfo();
+    queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + 
+                apiKey +
+                "&q=" + p +
+                "&limit=" + limit +
+                "&offset=0&rating=" + rating + "&lang=en";
     $("#buttonDisplay").append(addButton(p));
 })
 
+//give button a name from pinput
 function addButton(name){
-    var html = `<button class = " btn btn-info" value = "${queryUrl}" onclick=displyGif()>${name}</button>`;
+    var html = `<button class = " btn btn-info" value = "${queryUrl}" onclick="displayGif()">${name}</button>`;
     return html;
 }
-
-function displayGif(queryUrl){
-    // getButtonInfo();
-    // queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + 
-    //             apiKey +
-    //             "&q=" + q +
-    //             "&limit=" + limit +
-    //             "&rating=" + rating + "lang=en";
+$("#a").on("click",function(){
+    displayGif("https://api.giphy.com/v1/gifs/search?api_key=4tk6Du0rBvIooneYCYnhNOPkGvcLUnET&q=cat%20and%20dog&limit=25&offset=0&rating=Y&lang=en");
+})
+function displayGif(){
+    queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + 
+                apiKey +
+                "&q=" + p +
+                "&limit=" + limit +
+                "&offset=0&rating=" + rating + "&lang=en";
+    debugger;
     $.ajax({
         url: queryUrl,
-        method: 'GET'
+        method: "GET"
     }).then(function(response){
         for(let i = 0; i < parseInt(limit); i++){
+            //for(let i = 0; i < 25; i++){
             var results = response.data;
             var animalDiv = `<div>`
                             + `<p>${results[i].title}</p>`
                             + `<img src = "${results[i].images.fixed_width.url} ">`
                             + `</div>`;
-            $("#imageDisplay").preppend(animalDiv);
+            $("#imageDisplay").prepend(animalDiv);
         }
+        console.log(response);
     })
 }
